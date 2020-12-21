4a39fe28c5de7558edee695e3563a4c179a83cf8
Ekaterina Rys <e.rys@semrush.com>
2020-12-22 00:41:20 +0300

    Add startap file to run application, complete README.me instruction, fixed pre-commit checks

--- README.md
+++ README.md
@@ -38,3 +38,19 @@ _Connect to someinternalhost through VPN:_
 
 _Connect to Pritunl web-interface:_
 https://84.201.175.7.sslip.io/login
+___
+testapp_IP = 84.201.131.18 \
+testapp_port = 9292 \
+
+_How to install Monolith Reddit application_
+- install and run only one *startup_app.sh* file
+- wait about 2 mim
+- as a result:
+1. VM instance will be created
+2. Application will be run with address IP_VM:9292
+
+_How it works_
+- Startup file helps to create yc VM instance with unique configuration and pre-install custom scripts
+1. There are two installation scripts: install_ruby.sh and install_mongo.sh for ruby and mongo components
+2. There is deploy.sh script that configures installed packages and enables -demons- daemons on the cloud
+3. Also we have build_app.sh config to combine all previous parts in one image
---
+++ startup_cloud.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+RSA_KEY=$(cat ~/.ssh/appuser.pub)
+
+cat > metadata.yaml << EOM
+#cloud-config
+disable_root: true
+timezone: Europe/Moscow
+repo_update: true
+repo_upgrade: true
+apt:
+  preserve_sources_list: true
+
+users:
+    - default
+    - name: yc-user
+      sudo: ALL=(ALL) NOPASSWD:ALL
+      shell: /bin/bash
+      ssh-authorazed-keys:
+        - $RSA_KEY
+
+runcmd:
+  - curl https://raw.githubusercontent.com/Otus-DevOps-2020-11/kat-djonson_infra/cloud-testapp/build_app.sh | bash
+EOM
+
+#yandex cloud instance creating
+yc compute instance create \
+  --name reddit-app \
+  --hostname reddit-app \
+  --memory=4 \
+  --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
+  --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
+  --metadata serial-port-enable=1 \
+  --metadata-from-file user-data=./metadata.yaml
+
+rm -f metadata.yaml
